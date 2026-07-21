147c9c5caad630a3cc8ec2cf833b7977de0c9c70
MAHADEVAN-007 <raman007mahadevan@gmail.com>
2026-07-21 20:20:12 +0530

    agent vad

--- agent.py
+++ agent.py
@@ -200,7 +200,8 @@ async def entrypoint(ctx: JobContext):
             send_completion_event=True,
             temperature=0.1,
         ),
-        vad=silero.VAD.load(),
+        vad=None,
+        # vad=silero.VAD.load(),
         turn_handling=agents.TurnHandlingOptions(
             # turn_detection=inference.TurnDetector(),
             turn_detection="stt",

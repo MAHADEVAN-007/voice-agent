489be294a678dff8c216cb1aab848ed7edfe6d52
MAHADEVAN-007 <raman007mahadevan@gmail.com>
2026-07-21 20:39:48 +0530

    agent vad correct typo

--- agent.py
+++ agent.py
@@ -200,7 +200,7 @@ async def entrypoint(ctx: JobContext):
             send_completion_event=True,
             temperature=0.1,
         ),
-        svad=silero.VAD.load(),
+        vad=silero.VAD.load(),
         turn_handling=agents.TurnHandlingOptions(
             # turn_detection=inference.TurnDetector(),
             turn_detection="stt",
